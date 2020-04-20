c44c8f5717f1516cd043232069cc7982d8c4ba1d
haveagood@naver.com <haveagood@naver.com>
2020-04-20 12:02:07 +0900

    [BE] Docs : 백엔드 개발 디렉토리 생성
    - 백엔드 개발 디렉토리 생성하였습니다.
    
    -------------------------
    -------------------------

---
+++ BE/src/test/java/com/sidedish4/codesquad/sidedish/SidedishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidedish4.codesquad.sidedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

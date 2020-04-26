124e1621b11d99747cf706310e7bf18d6ccd3e42
Elllin <noto01@naver.com>
2020-04-26 13:12:28 +0900

    [#36] carousel 스타일 추가

--- FE/sidedish/src/conponents/Main/Carousel.js
+++ FE/sidedish/src/conponents/Main/Carousel.js
@@ -1,76 +1,188 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {ThemeProvider} from 'styled-components';
 import Slider from 'react-slick';
+import theme from '../theme';
+import './Carousel.css';
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-const CarouselWrap = styled.section`
-  width:100%;
-  height: 370px;
-  /* padding:20px 0; */
-  background-color:red;
-  /* display:flex; */
-  /* justify-content: space-between; */
-  /* align-items: center; */
-`
 function Carousel() {
+
+  const ArrowLeft = (props) => (
+    <button
+        {...props}
+        className={'s.prev'}/>
+);
+const ArrowRight = (props) => (
+    <button
+        {...props}
+        className={'s.next'}/>
+);
   const slideSetting = {
-    slide: 'li',
+
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
-    arrows : true
+    arrows : true,
+    prevArrow: <ArrowLeft />,
+    nextArrow: <ArrowRight />,
   }
 
- const Ul = styled.ul`
 
+
+
+ const SliderWrap = styled.div`
+ width:960px;
+ margin:0 auto;
+
+ 
 `
- const Li = styled.li`
-  width:90%;
-  /* background-color:blue; */
+ const Itme = styled.div`
+  width:  215px; 
+  padding: 0px 15px 8px;
+  min-height: 360px;
+  text-align:center;
 `
 
 
-const ItemArea = styled.div`
+const ImgArea = styled.div`
     height: 215px;
     background: #f2f2f2;
     border-radius: 50%;
     overflow:hidden;
-
 `
 
 const Img = styled.img`
   height:100%;
   width:100%;
- 
 `
+
+const ItemContent = styled.dl`
+  padding: 16px 10px 12px 10px;
+  text-align: center;
+  overflow: hidden;
+`
+const ItemTitle = styled.dt`
+ /* text-overflow:hidden; */
+  margin-bottom: 5px;
+  font-size: 16px;
+  letter-spacing: -0.07em;
+  overflow: hidden;
+
+`
+
+const ItemDesc = styled.dd`
+margin-bottom: 4px;
+font-size: 14px;
+    color: #666;
+    min-height: 16px;
+    overflow: hidden;
+`
+
+const SellingPrice = styled.p`
+  color: ${props => props.theme.sellingColor};
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 1;
+  margin-right: 10px;
+  span{
+    color: ${props => props.theme.sellingColor};
+    font-size: 14px;
+  }
+`
+const OriginPrice = styled.p`
+  color: ${props => props.theme.originColor};
+  text-decoration:line-through;
+`
+
+const Price = styled.dd`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+
 
   
   return(
-    <CarouselWrap>
+      <ThemeProvider theme={theme}>
+ 
+        {/* <CarouselWrap> */}
+    <SliderWrap>
     <Slider {...slideSetting}>
-     <ul>
-       <Li>
-         <ItemArea>
+       <Itme>
+         <ImgArea>
            <Img src="https://web.archive.org/web/20190122062652im_/https://cdn.bmf.kr/_data/product/I9E9A/ab4c422855875181ede52f6ac1954fde.jpg" alt="" />
-        </ItemArea>
-        <dl>
-           <dt class="item__title">[잡밥의완성]두부시래기조림 300g</dt>
-           <dd class="item__desc">칼칼해서 더 좋아요</dd>
-           <dd class="item__price">
-             <p class="price__selling">3,050<span>원</span></p>
-             <p class="price__origin">4,050<span>원</span></p>
-           </dd>
-         </dl>
-      </Li>
-  
-     </ul>
-
+        </ImgArea>
+        <ItemContent>
+           <ItemTitle >[잡밥의완성]두부시래기조림 300g</ItemTitle>
+           <ItemDesc>칼칼해서 더 좋아요</ItemDesc>
+           <Price>
+             <SellingPrice>3,050<span>원</span></SellingPrice>
+             <OriginPrice>4,050</OriginPrice>
+           </Price>
+         </ItemContent>
+      </Itme>
+       <Itme>
+         <ImgArea>
+           <Img src="https://web.archive.org/web/20190122062652im_/https://cdn.bmf.kr/_data/product/I9E9A/ab4c422855875181ede52f6ac1954fde.jpg" alt="" />
+        </ImgArea>
+        <ItemContent>
+           <ItemTitle >[잡밥의완성]두부시래기조림 300g</ItemTitle>
+           <ItemDesc>칼칼해서 더 좋아요</ItemDesc>
+           <Price>
+             <SellingPrice>3,050<span>원</span></SellingPrice>
+             <OriginPrice>4,050</OriginPrice>
+           </Price>
+         </ItemContent>
+      </Itme>
+       <Itme>
+         <ImgArea>
+           <Img src="https://web.archive.org/web/20190122062652im_/https://cdn.bmf.kr/_data/product/I9E9A/ab4c422855875181ede52f6ac1954fde.jpg" alt="" />
+        </ImgArea>
+        <ItemContent>
+           <ItemTitle >[잡밥의완성]두부시래기조림 300g</ItemTitle>
+           <ItemDesc>칼칼해서 더 좋아요</ItemDesc>
+           <Price>
+             <SellingPrice>3,050<span>원</span></SellingPrice>
+             <OriginPrice>4,050</OriginPrice>
+           </Price>
+         </ItemContent>
+      </Itme>
+       <Itme>
+         <ImgArea>
+           <Img src="https://web.archive.org/web/20190122062652im_/https://cdn.bmf.kr/_data/product/I9E9A/ab4c422855875181ede52f6ac1954fde.jpg" alt="" />
+        </ImgArea>
+        <ItemContent>
+           <ItemTitle >[잡밥의완성]두부시래기조림 300g</ItemTitle>
+           <ItemDesc>칼칼해서 더 좋아요</ItemDesc>
+           <Price>
+             <SellingPrice>3,050<span>원</span></SellingPrice>
+             <OriginPrice>4,050</OriginPrice>
+           </Price>
+         </ItemContent>
+      </Itme>
+       <Itme>
+         <ImgArea>
+           <Img src="https://web.archive.org/web/20190122062652im_/https://cdn.bmf.kr/_data/product/I9E9A/ab4c422855875181ede52f6ac1954fde.jpg" alt="" />
+        </ImgArea>
+        <ItemContent>
+           <ItemTitle >[잡밥의완성]두부시래기조림 300g</ItemTitle>
+           <ItemDesc>칼칼해서 더 좋아요</ItemDesc>
+           <Price>
+             <SellingPrice>3,050<span>원</span></SellingPrice>
+             <OriginPrice>4,050</OriginPrice>
+           </Price>
+         </ItemContent>
+      </Itme>
+ 
    </Slider>
-  </CarouselWrap>
+      </SliderWrap>
+  {/* </CarouselWrap> */}
+     </ThemeProvider>
   )
 }
 
--- FE/sidedish/src/conponents/Main/Main.js
+++ FE/sidedish/src/conponents/Main/Main.js
@@ -4,8 +4,8 @@ import Carousel from './Carousel';
 import Title from './Title';
 
 const MainWrap = styled.main`
-    margin: 0 auto;
-    width: 960px;
+    /* margin: 0 auto;
+    width: 960px; */
 `
 
 function Main (){
